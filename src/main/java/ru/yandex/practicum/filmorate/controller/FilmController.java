e5564471f10debec6d75f80730ec8dab27962413
egopv <egop.valuev@yandex.ru>
2025-06-12 01:16:18 +0300

    started UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -30,8 +31,50 @@ public class FilmController {
             throw new ValidationException("Превышено максимальное количество символов. ");
         }
         if (!LocalDate.of(1895,12,28).isBefore(film.getReleaseDate())) {
-
+            throw new ValidationException("Дата не валидна");
         }
+        if (film.getDuration() == null || film.getDuration().isNegative() || film.getDuration().isZero()){
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            throw new ValidationException("ID должен быть указан");
+        }
+        // проверяем выполнение необходимых условий
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getDescription().length() > 200) {
+                throw new ValidationException("Превышено максимальное количество символов. ");
+            }
+            if (!LocalDate.of(1895, 12, 28).isBefore(newFilm.getReleaseDate())) {
+                throw new ValidationException("Дата не валидна");
+            }
+            if (newFilm.getDuration().isNegative() || newFilm.getDuration().isZero()) {
+                throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+            }
+            // Обновление остальных полей
+            if (newFilm.getName() != null) {
+                oldFilm.setName(newFilm.getName());
+            }
+            if (newFilm.getDescription() != null) {
+                oldFilm.setDescription(newFilm.getDescription());
+            }
+            if (newFilm.getReleaseDate() != null) {
+                oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            }
+            if (newFilm.getDuration() != null) {
+                oldFilm.setDuration(newFilm.getDuration());
+            }
+            return oldFilm;
+        }
+
+        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
     }
 
     private long getNextId() {
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,103 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    // Вспомогательный метод для генерации ID нового пользователя
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        // Проверка обязательных условий
+        if (user.getEmail() == null || user.getEmail().isBlank() || user.getEmail().contains("@")) {
+            throw new ValidationException("Email должен быть указан");
+        }
+        // Проверка уникальности email
+        boolean emailExists = users.values()
+                .stream()
+                .anyMatch(u -> u.getEmail().equals(user.getEmail()));
+        if (emailExists) {
+            throw new ValidationException("Этот email уже используется");
+        }
+        if (user.getLogin() == null) {
+            throw new ValidationException("Логин обязателен");
+        }
+
+        if (user.getLogin().isBlank()) {
+            throw new ValidationException("Логин не может быть пустым или состоять из пробелов");
+        }
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+
+        // Установка дополнительных полей
+        user.setId(getNextId());
+        // Сохранение пользователя
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        // Проверка обязательных полей
+        if (newUser.getId() == null) {
+            throw new ValidationException("ID должен быть указан");
+        }
+
+        // Поиск существующего пользователя
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            // Проверка и обновление email
+            if (newUser.getEmail() != null && !newUser.getEmail().equals(oldUser.getEmail())) {
+                boolean emailExists = users.values()
+                        .stream()
+                        .anyMatch(u -> u.getEmail().equals(newUser.getEmail()));
+                if (emailExists) {
+                    throw new DuplicatedDataException("Этот email уже используется");
+                }
+                oldUser.setEmail(newUser.getEmail());
+            }
+
+            // Обновление остальных полей
+            if (newUser.getUsername() != null) {
+                oldUser.setUsername(newUser.getUsername());
+            }
+
+            if (newUser.getPassword() != null) {
+                oldUser.setPassword(newUser.getPassword());
+            }
+
+            return oldUser;
+        }
+
+        throw new NotFoundException("Пользователь с ID = " + newUser.getId() + " не найден");
+    }
+}
 }
